25210a3851dbb8b581262c6827fe319995cba10a
Everton Redivo <51525549+rdv-redivo@users.noreply.github.com>
2019-07-21 17:33:28 -0300

    Update tower_of_hanoi.py

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -1,1 +1,16 @@
+#Tower of Hanoi - Recursive Method
 
+def hanoi(n, origem, aux, destino):
+    
+    if (n == 1):
+        
+        print('Mova de', origem,'para', destino, '.')
+    
+    else:
+            
+        hanoi(n-1, origem, destino, aux)
+        hanoi(1, origem, aux, destino)
+        hanoi(n-1, aux, origem, destino)
+        
+        
+#hanoi(10 , 'P1', 'P2', 'P3')
